24e540b47f2c456e30a2ff4a761a612b8e2ac0f5
Jon Kinney <jonkinney@gmail.com>
2025-08-29 16:18:46 -0500

    Remove asahi-alarm/widevine for x86

--- install/packages.sh
+++ install/packages.sh
@@ -136,7 +136,6 @@ $PKG_MANAGER -Syy --noconfirm --needed \
 
 if [ -z "$OMARCHY_ARM" ]; then
   $PKG_MANAGER -S --noconfirm --needed \
-    asahi-alarm/widevine \
     1password-beta \
     1password-cli \
     localsend \
