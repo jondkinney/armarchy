634afce91e98c8667b091c5c88cb2c5b5883d36b
Jon Kinney <jonkinney@gmail.com>
2025-09-01 20:58:56 -0500

    ARM specific pacman.conf

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -2,13 +2,12 @@ if [[ -n ${OMARCHY_ONLINE_INSTALL:-} ]]; then
   # Install build tools
   sudo pacman -S --needed --noconfirm base-devel
 
-  # Configure pacman
-  sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
-
+  # Configure pacman - use ARM-specific configs on ARM systems
   if [[ "$OMARCHY_ARM" == "true" ]]; then
-    # Use ARM-specific mirrorlist on ARM systems
+    sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf.arm /etc/pacman.conf
     sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist.arm /etc/pacman.d/mirrorlist
   else
+    sudo cp -f ~/.local/share/omarchy/default/pacman/pacman.conf /etc/pacman.conf
     sudo cp -f ~/.local/share/omarchy/default/pacman/mirrorlist /etc/pacman.d/mirrorlist
   fi
 fi
