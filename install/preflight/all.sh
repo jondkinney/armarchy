9f3f7e72e631f562c52a943c06d8abddc23bfaeb
Jon Kinney <jonkinney@gmail.com>
2025-09-22 15:00:12 -0500

    Reorder pacman after OMARCHY_ARM export

--- install/preflight/all.sh
+++ install/preflight/all.sh
@@ -1,7 +1,6 @@
 source $OMARCHY_INSTALL/preflight/guard.sh
 source $OMARCHY_INSTALL/preflight/begin.sh
 run_logged $OMARCHY_INSTALL/preflight/show-env.sh
-run_logged $OMARCHY_INSTALL/preflight/pacman.sh
 
 arch=$(uname -m)
 if [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
@@ -9,6 +8,7 @@ if [[ "$arch" == "aarch64" || "$arch" == "arm64" ]]; then
   run_logged $OMARCHY_INSTALL/preflight/asahi.sh
 fi
 
+run_logged $OMARCHY_INSTALL/preflight/pacman.sh
 run_logged $OMARCHY_INSTALL/preflight/migrations.sh
 run_logged $OMARCHY_INSTALL/preflight/first-run-mode.sh
 run_logged $OMARCHY_INSTALL/preflight/disable-mkinitcpio.sh
