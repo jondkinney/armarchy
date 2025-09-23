516c15c4bcc63856bf7a3e9f7b5fb3720e07c743
Jon Kinney <jonkinney@gmail.com>
2025-09-23 06:10:41 -0500

    remove conditional for yaru

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -1,8 +1,6 @@
-if [[ -d "/usr/share/icons/Yaru" ]]; then # Set icon theme and links only if Yaru is installed
-  # Set links for Nautilius action icons
-  sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
-  sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
-fi
+# Set links for Nautilius action icons
+sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-previous-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-previous-symbolic.svg
+sudo ln -snf /usr/share/icons/Adwaita/symbolic/actions/go-next-symbolic.svg /usr/share/icons/Yaru/scalable/actions/go-next-symbolic.svg
 
 # Setup theme links
 mkdir -p ~/.config/omarchy/themes
