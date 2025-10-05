d5618a61a4b5f009a38249fefb482f68e7165c54
Jon Kinney <jonkinney@gmail.com>
2025-10-05 18:23:44 -0500

    Shallow, quiet clone for non-master branches
    We don't need to get a fork's entire repository, just fetch the specific
    branch they want to install custom

--- boot.sh
+++ boot.sh
@@ -67,21 +67,18 @@ if [[ $EUID -eq 0 ]]; then
   exit 0 # exit to not run the rest of the script, and avoid cloning as root
 fi
 
-echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
-
 if [[ -n $OMARCHY_RESUME_INSTALL ]]; then
   echo -e "\n\e[32mResuming Omarchy installation from where it left off...\e[0m\n"
 else
   echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
 
   rm -rf ~/.local/share/omarchy/
-  git clone "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy >/dev/null
 
   if [[ $OMARCHY_REF != "master" ]]; then
-    echo -e "\e[32mUsing branch: $OMARCHY_REF\e[0m"
-    cd ~/.local/share/omarchy
-    git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
-    cd -
+    echo -e "\n\e[32mUsing branch: $OMARCHY_REF (shallow clone)\e[0m"
+    git clone --quiet --depth 1 --branch "${OMARCHY_REF}" "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy
+  else
+    git clone --quiet "https://github.com/${OMARCHY_REPO}.git" ~/.local/share/omarchy
   fi
 fi
 
