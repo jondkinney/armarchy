0a7014ed5860998f96e5c56f3722874c62dda25f
Jon Kinney <jonkinney@gmail.com>
2025-11-23 22:15:28 -0600

    Fix pinta

--- install/arm_install_scripts/pinta.sh
+++ install/arm_install_scripts/pinta.sh
@@ -6,15 +6,15 @@ if command -v pinta &>/dev/null; then
   return 0
 fi
 
-# Install .NET SDK and runtime from the generic dotnet-core-bin package
-# Note: Using .NET 10 (latest) instead of .NET 8 because dotnet-core-8.0-bin
+# Install .NET 10 SDK and runtime from dotnet-core-10.0-bin split package
+# Note: Using .NET 10 instead of .NET 8 because dotnet-core-8.0-bin
 # has a broken dependency on non-existent 'netstandard-targeting-pack' package
-# The generic packages provide dotnet-sdk and dotnet-runtime virtual packages
-# that pinta-git requires
+# The 10.0 packages provide unversioned 'dotnet-runtime' that pinta-git requires
+# (the 8.0 packages only provide 'dotnet-runtime-8.0' which doesn't satisfy the dep)
 "$OMARCHY_PATH/bin/omarchy-aur-install" --makepkg-flags="--needed" \
-  dotnet-host-bin \
-  dotnet-runtime-bin \
-  dotnet-sdk-bin
+  dotnet-host-10.0-bin \
+  dotnet-runtime-10.0-bin \
+  dotnet-sdk-10.0-bin
 
 # Now install Pinta - its deps will be satisfied by the above packages
 "$OMARCHY_PATH/bin/omarchy-aur-install" --makepkg-flags="--needed" pinta-git
