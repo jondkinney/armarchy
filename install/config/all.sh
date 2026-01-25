893fbc6c00f7d8402f290890a829b1a4dcc4441a
Jon Kinney <jonkinney@gmail.com>
2026-01-25 00:30:43 -0600

    Configure 8gb swap for Pi 500+

--- install/config/all.sh
+++ install/config/all.sh
@@ -59,6 +59,7 @@ run_logged $OMARCHY_INSTALL/config/hardware/apple/fix-t2.sh
 
 run_logged $OMARCHY_INSTALL/config/hardware/fix-bcm43xx.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-pi500-wifi.sh
+run_logged $OMARCHY_INSTALL/config/hardware/swap-pi500.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-surface-keyboard.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-yt6801-ethernet-adapter.sh
 run_logged $OMARCHY_INSTALL/config/hardware/fix-synaptic-touchpad.sh
